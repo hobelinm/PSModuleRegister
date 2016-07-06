663537852864428309c48ef15cc15a4c37bcbe50
Hugo Belin Melgoza <hugobel@microsoft.com>
2016-07-05 23:27:43 -0700

    Additional logging

--- index.js
+++ index.js
@@ -4,6 +4,17 @@ var fs = require('fs');
 var program = require('commander');
 var exec = require('child_process').exec;
 
+function runCommand (cmd) {
+    console.log("About to execute: " + cmd);
+    exec(cmd, function(err, stdout, stderr) {
+        console.log(stdout);
+        if (err) {
+            errorFound = true;
+            console.warn(stderr);
+        }
+    });
+}
+
 program
     .version('1.0.0')
     .option('-i, --install <ps-module-package>', 'npm install PowerShell module')
@@ -37,14 +48,4 @@ if (program.register) {
     }
     
     process.exit(0);
-}
-
-function runCommand (cmd) {
-    exec(cmd, function(err, stdout, stderr) {
-        console.log(stdout);
-        if (err) {
-            errorFound = true;
-            console.warn(stderr);
-        }
-    });
 }
